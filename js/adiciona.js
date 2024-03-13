d31157ad412709e470d3cee174ea39d34b7935d5
Erick Ayres <darshu_14@hotmail.com>
2024-03-13 00:39:55 -0300

    Fix: corrige código javascript e finaliza o processo de carregamento dos itens na página.

--- js/adiciona.js
+++ js/adiciona.js
@@ -1,4 +1,34 @@
 lista_categoria = document.getElementById('lista-categoria');
+var cadastros = '';
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  getJSON().then(data=>{
+    cadastros = data;
+    
+    for(categoria in cadastros){
+
+      switch(categoria.toLowerCase()){
+        case 'lanches' : icon = 'hamburguer01.png';break;
+        case 'pizzas'  : icon = 'pizza01.png';break;
+        case 'marmitex': icon = 'comida01.png';break;
+        case 'lasanhas': icon = 'lasanha01.png';break;
+        case 'pastel'  : icon = 'pastel01.png';break;
+        default: icon = 'outros.png';
+      }
+
+      adicionaCategoria(categoria,icon);
+
+      cadastros[categoria].forEach((loja, i)=>{
+        adicionaItem(categoria,loja,i);
+      });
+    }
+
+    adicionaScript();
+
+  });
+  
+});
 
 function adicionaCategoria(categoria,img){
     titulo = categoria[0].toUpperCase() + categoria.slice(1);
@@ -23,4 +53,33 @@ function adicionaCategoria(categoria,img){
   </div>
     `
     lista_categoria.innerHTML += item;
+}
+
+function adicionaItem(categoria, loja, index){
+  lista = document.querySelector("#lista-"+categoria)
+
+  item = `
+      <li style="--clr:#25D366;--i:${index}">
+        <a href="https://wa.me/55${loja['whatsapp']}">
+          <div class="iconBx"><i class="fa-brands fa-whatsapp"></i></div>
+          <P>${loja['nome']}</P>
+        </a>
+      </li>
+  `
+
+  lista.innerHTML += item;
+}
+
+async function getJSON(){
+  var dados = '';
+  const response = await fetch('../bd/lugares.json');
+  const data = await response.json();
+  dados = data;
+  return dados;
+}
+
+function adicionaScript(){
+  script = document.createElement('script');
+  script.src = 'js/script.js';
+  document.head.appendChild(script);
 }
